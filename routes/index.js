6d12191339bab58d77e8ca58d1b1bf28b782acc3
Schipsi <thibaut.juston@thetribe.io>
2022-01-06 17:57:47 +0100

    Feat: change url

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,12 @@ var turboExpress = require('express');
 var turboRouter = turboExpress.Router();
 var turboCheerio = require('cheerio');
 const turboRequest = require('request');
+const util = require('util');
 
-
+const asyncTurboGet = util.promisify(turboRequest.get);
 
 const fetchData = async (url) => {
-  let response = await turboRequest.get(url);
-  let $ = turboCheerio.load(response);
-  return {
-    title: $('title').text(),
-  }
+  return await asyncTurboGet(url);
 }
 
 turboRouter.get('/', async function (req, res) {
@@ -19,9 +16,15 @@ turboRouter.get('/', async function (req, res) {
     res.render('error', { error: { status: '404', stack: 'bolostack' }, message: 'turbolosse' });
   }
 
-  const {title} = await fetchData('https://www.infoconcert.com/');
+  const response = await fetchData(`https://www.infoconcert.com/recherche-concert.html?motclef=${turboSearch}`);
 
-  res.render('index', { turboTitle: title, turboContent: turboSearch });
+  let $ = turboCheerio.load(response.body);
+  const title = $('title').text();
+  const firstResultName = $('html').find('.search-results-cat:first-child .results-line:first-child a').text();
+  const firstResultUrl = $('html').find('.search-results-cat:first-child .results-line:first-child a').attr('href');
+  console.log(firstResultName);
+  console.log(firstResultUrl);
+  res.render('index', { turboTitle: title, turboContent: `${firstResultName} ${firstResultUrl}` });
 });
 
 module.exports = turboRouter;
